3525875fa0bd2fc04336fba6d636032ba44c7eb8
Lyana <162919159+beeulya@users.noreply.github.com>
2025-01-26 12:33:21 +0300

    Lab3

---
+++ lab3.py
@@ -0,0 +1,51 @@
+class Book:
+    """ Базовый класс книги. """
+    def __init__(self, name: str, author: str):
+        self._name = name
+        self._author = author
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def author(self):
+        return self._author
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        self._pages = pages
+
+    @property
+    def pages(self):
+        return self._pages
+
+    @pages.setter
+    def pages(self, pages: int):
+        if pages <= 0:
+            raise ValueError("Количество страниц должно быть положительным")
+        self._pages = pages
+
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        self._duration = duration  # Изменение атрибута на защищенный (_duration)
+
+    @property
+    def duration(self):
+        return self._duration
+
+    @duration.setter
+    def duration(self, duration: float):
+        if duration <= 0:
+            raise ValueError("Продолжительность должна быть положительной")
+        self._duration = duration
